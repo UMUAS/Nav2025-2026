c7f79f585538789e27759d07e3568a21f99d9d8e
UrielMiller <umiller101@gmail.com>
2026-01-11 14:53:07 -0600

    Improved opticalflow.py

--- opticalflow.py
+++ opticalflow.py
@@ -1,82 +1,57 @@
 import numpy as np
 import cv2 as cv
 
-cap = cv.VideoCapture('vid/lawn_watering.mp4')
-
-out_w = 1080
-c = 1
+widths = [1080, 720, 480]
+out_w = widths[2]
 
 def rescale(frame):
     global out_w
     h, w = frame.shape[:2]
     ratio = (out_w/w)
     return cv.resize(frame, (out_w, int(h*ratio)))
- 
-# params for ShiTomasi corner detection
-feature_params = dict( maxCorners = 100, qualityLevel = 0.3, minDistance = 7, blockSize = 7 )
- 
-# Parameters for lucas kanade optical flow
-lk_params = dict(winSize  = (15, 15), maxLevel = 2, criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
 
-# Create some random colors
-color = np.random.randint(0, 255, (100, 3))
- 
-# Take first frame and find corners in it
-ret, src_frame = cap.read()
-old_frame = rescale(src_frame)
-old_gray = cv.extractChannel(cv.cvtColor(old_frame, cv.COLOR_BGR2HSV), 2)
-p0 = cv.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
- 
-# Create a mask image for drawing purposes
-mask = np.zeros_like(old_frame)
+def main():
+    cv.namedWindow('Frame')
 
-cv.namedWindow('Frame')
+    # cv.createTrackbar('Threshold', 'Frame', 128, 255, lambda _: None)
+    # cv.createTrackbar('Blur', 'Frame', 3, 7, lambda _: None)
 
-cv.createTrackbar('Threshold', 'Frame', 128, 255, lambda _: None)
-cv.createTrackbar('Blur', 'Frame', 3, 7, lambda _: None)
- 
-while(1):
-    ret, src_frame = cap.read()
-    if not ret:
-        print('No frames grabbed!')
-        break
+    cap = cv.VideoCapture('vid/lawn_watering.mp4')
 
-    frame = rescale(src_frame)
- 
-    # frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    frame_gray = cv.extractChannel(cv.cvtColor(frame, cv.COLOR_BGR2HSV), 2)
+    ret, src0 = cap.read()
+    frame0 = rescale(src0)
 
-    thresh_val = cv.getTrackbarPos('Threshold', 'Frame')
-    blur_val = cv.getTrackbarPos('Blur', 'Frame')
+    prvs = cv.cvtColor(frame0, cv.COLOR_BGR2GRAY)
+    hsv = np.zeros_like(frame0)
+    hsv[..., 1] = 255
 
-    blur = cv.blur(frame_gray, (blur_val, blur_val))
-    _, modif = cv.threshold(blur, thresh_val, 255, cv.THRESH_BINARY)
- 
-    # calculate optical flow
-    p1, st, err = cv.calcOpticalFlowPyrLK(old_gray, modif, p0, None, **lk_params)
- 
-    # Select good points
-    if p1 is not None:
-        good_new = p1[st==1]
-        good_old = p0[st==1]
- 
-    # draw the tracks
-    for i, (new, old) in enumerate(zip(good_new, good_old)):
-        a, b = new.ravel()
-        c, d = old.ravel()
-        mask = cv.line(mask, (int(a), int(b)), (int(c), int(d)), color[i].tolist(), 2)
-        frame = cv.circle(frame, (int(a), int(b)), 5, color[i].tolist(), -1)
-    img = cv.add(frame, mask)
- 
-    cv.imshow('Frame', img)
-    cv.imshow('Frame2', modif)
-    k = cv.waitKey(30) & 0xff
-    if k == 27:
-        break
- 
-    # Now update the previous frame and previous points
-    # old_gray = frame_gray.copy()
-    old_gray = modif.copy()
-    p0 = good_new.reshape(-1, 1, 2)
- 
-cv.destroyAllWindows()
+    while True:
+        ret, src_frame = cap.read()
+        if not ret:
+            print('No frames grabbed!')
+            break
+
+        frame = rescale(src_frame)
+        nxt = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        flow = cv.calcOpticalFlowFarneback(prvs, nxt, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+        mag, ang = cv.cartToPolar(flow[..., 0], flow[..., 1])
+        hsv[..., 0] = ang*180/np.pi/2
+        hsv[..., 2] = cv.normalize(mag, None, 0, 255, cv.NORM_MINMAX)
+        bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
+        
+        # thresh_val = cv.getTrackbarPos('Threshold', 'Frame')
+        # blur_val = cv.getTrackbarPos('Blur', 'Frame')
+    
+        cv.imshow('Frame', frame)
+        cv.imshow('Frame2', bgr)
+        # cv.imshow('Frame2', frame_gray)
+        k = cv.waitKey(30) & 0xff
+        if k == 27:
+            break
+
+        prvs = nxt
+    
+    cv.destroyAllWindows()
+
+if(__name__ == '__main__'):
+    main()
