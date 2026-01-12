44e4fb6a182c02de96eb71207964193b81e4196b
UrielMiller <umiller101@gmail.com>
2026-01-11 19:42:20 -0600

    Improved opticalflow.py again

--- opticalflow.py
+++ opticalflow.py
@@ -13,10 +13,13 @@ def rescale(frame):
 def main():
     cv.namedWindow('Frame')
 
-    # cv.createTrackbar('Threshold', 'Frame', 128, 255, lambda _: None)
+    cv.createTrackbar('Max. Sat', 'Frame', 80, 255, lambda _: None)
+    cv.createTrackbar('Min. Val', 'Frame', 140, 255, lambda _: None)
     # cv.createTrackbar('Blur', 'Frame', 3, 7, lambda _: None)
 
-    cap = cv.VideoCapture('vid/lawn_watering.mp4')
+    # cap = cv.VideoCapture('vid/lawn_watering.mp4')
+    # cap = cv.VideoCapture('vid/sprinkler.mp4')
+    cap = cv.VideoCapture('vid/apt_fire.mp4')
 
     ret, src0 = cap.read()
     frame0 = rescale(src0)
@@ -28,10 +31,13 @@ def main():
     while True:
         ret, src_frame = cap.read()
         if not ret:
-            print('No frames grabbed!')
-            break
+            cap.set(cv.CAP_PROP_POS_FRAMES, 0)
+            continue
+            # print('No frames grabbed!')
+            # break
 
         frame = rescale(src_frame)
+
         nxt = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         flow = cv.calcOpticalFlowFarneback(prvs, nxt, None, 0.5, 3, 15, 3, 5, 1.2, 0)
         mag, ang = cv.cartToPolar(flow[..., 0], flow[..., 1])
@@ -39,15 +45,39 @@ def main():
         hsv[..., 2] = cv.normalize(mag, None, 0, 255, cv.NORM_MINMAX)
         bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
         
-        # thresh_val = cv.getTrackbarPos('Threshold', 'Frame')
+        minval_val = cv.getTrackbarPos('Min. Val', 'Frame')
+        maxsat_val = cv.getTrackbarPos('Max. Sat', 'Frame')
         # blur_val = cv.getTrackbarPos('Blur', 'Frame')
+
+        frame_hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+        _, sat = cv.threshold(cv.extractChannel(frame_hsv, 1), maxsat_val, 255, cv.THRESH_BINARY_INV)
+        _, val = cv.threshold(cv.extractChannel(frame_hsv, 2), minval_val, 255, cv.THRESH_BINARY)
+
+        comb = cv.bitwise_and(sat, val)
+        
+        masked_bgr = cv.bitwise_and(bgr, bgr, mask=comb)
+
+        # modif = thresh
     
         cv.imshow('Frame', frame)
-        cv.imshow('Frame2', bgr)
-        # cv.imshow('Frame2', frame_gray)
+        cv.imshow('Value', val)
+        cv.imshow('Saturation', sat)
+        cv.imshow('Value+Satruation', comb)
+        cv.imshow('Optical Flow', bgr)
+        cv.imshow('Masked Optical Flow', masked_bgr)
         k = cv.waitKey(30) & 0xff
         if k == 27:
             break
+        elif k == ord('s'):
+            cv.imwrite('frame.png', frame)
+            cv.imwrite('hsv_value.png', val)
+            cv.imwrite('hsv_saturation.png', sat)
+            cv.imwrite('value_satruation_blend.png', comb)
+            cv.imwrite('optical_flow.png', bgr)
+
+            # cv.imwrite('opticalfb.png', frame)
+            # cv.imwrite('opticalhsv.png', bgr)
 
         prvs = nxt
     
