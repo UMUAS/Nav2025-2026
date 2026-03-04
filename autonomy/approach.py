8cbbc5de09ed347a5b07d70f39dbad79727b2aa2
UrielMiller <umiller101@gmail.com>
2026-03-03 21:56:25 -0600

    Organized and cleaned branch

--- autonomy/approach.py
+++
@@ -1,6 +0,0 @@
-from pymavlink import mavutil
-import threading
-from typing import Callable, Optional, Tuple, List
-
-def approach():
-    ...
--- telemetry/send_telemetry.py
+++
@@ -1,4 +0,0 @@
-# TODO
-# - Send accuracies of YOLO and backup target detections
-# - Send RGB and depth maps with target's bounding box
-# - more?
